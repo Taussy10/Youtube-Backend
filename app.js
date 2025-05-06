eb31b6ffdb40df46a1fe185eee59ee11dcbcfff5
Tausif <tausif00100@gmail.com>
2025-05-06 20:46:08 +0530

    started server

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express"
+
+// intiate the app 
+const app = express()
+
+// Intialize the server
+app.get("/", (req, res) => (
+    // you have to send it to start server
+    // show somethng on browser 
+ res.send("Server")
+))
+
+// Listen to the server
+// (method )Application<Record<string, any>>.listen(
+//     port: number, hostname: string, backlog: number,
+//  callback?: (error?: Error) => void):
+// so this is a method will take port as number ,
+//  hostname callback
+// you don't have to return this 
+// make it async for time if it's take 
+// Then who will resolve it cause by async it become promise
+
+app.listen(PORT, async() => ( 
+    console.log(`Server is running on http://localhost:${PORT}`)
+        
+))
---
+++ routes/index.js
@@ -0,0 +1,2 @@
+// To creat rouer we need to import router 
+import {Router} from "express"
